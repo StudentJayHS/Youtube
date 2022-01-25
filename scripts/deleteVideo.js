a900b46b0f1636257efacaea568a01add7ef3f3a
Newbie <hugevory1998@naver.com>
2022-01-25 21:02:19 +0900

    add scroll function

--- scripts/deleteVideo.js
+++ scripts/deleteVideo.js
@@ -1,11 +1,16 @@
 const form = document.querySelectorAll(".form");
+let nowScrollTop = "";
 
+// 삭제를 했을 떄 스크롤 고정
+document.addEventListener('scroll', () => {
+    nowScrollTop = document.documentElement.scrollTop;
+})
 
 form.forEach(form => {
     form.addEventListener("submit", (event) => {
         event.preventDefault();
 
-        const delForm = event.target
+        const delForm = event.target;
         const videoId = delForm.getElementsByClassName('recodeVideoId')[0].value;
 
         // className 을 가져올 때 배열 0 번 또는 1번을 지정하지 않으면 다중 input 을 가져오는데 왜 그런걸까?
@@ -18,6 +23,7 @@ form.forEach(form => {
             body: JSON.stringify({videoId})
         }).then(response => response.json())
         .then(data => {
+            document.documentElement.scrollTop = nowScrollTop;
             delForm.remove();
         })
     })
